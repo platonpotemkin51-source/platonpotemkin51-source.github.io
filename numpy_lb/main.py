f4f913611b95ea4c2bdc24c2149b15d3c5096cc0
Platon Potemkin <platonpotemkin51@gmail.com>
2026-03-23 20:17:55 +0300

    123

--- numpy_lb/main.py
+++ numpy_lb/main.py
@@ -4,3 +4,312 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# ============================================================
+# 1. СОЗДАНИЕ И ОБРАБОТКА МАССИВОВ
+# ============================================================
+
+def create_vector():
+    """
+    Создать массив от 0 до 9.
+
+    Returns:
+        numpy.ndarray: Массив чисел от 0 до 9 включительно
+    """
+    # Подсказка: используйте np.arange(10)
+    return np.ones(9)
+
+
+def create_matrix():
+    """
+    Создать матрицу 5x5 со случайными числами [0,1].
+
+    Returns:
+        numpy.ndarray: Матрица 5x5 со случайными значениями от 0 до 1
+    """
+
+    return np.random.rand(5,5)
+
+
+def reshape_vector(vec):
+    """
+    Преобразовать (10,) -> (2,5)
+
+    Args:
+        vec (numpy.ndarray): Входной массив формы (10,)
+
+    Returns:
+        numpy.ndarray: Преобразованный массив формы (2, 5)
+    """
+
+    return np.reshape(vec, (2, 5))
+
+
+def transpose_matrix(mat):
+    """
+    Транспонирование матрицы.
+
+    Args:
+        mat (numpy.ndarray): Входная матрица
+
+    Returns:
+        numpy.ndarray: Транспонированная матрица
+    """
+
+    return np.transpose(mat)
+
+
+# ============================================================
+# 2. ВЕКТОРНЫЕ ОПЕРАЦИИ
+# ============================================================
+
+def vector_add(a, b):
+    """
+    Сложение векторов одинаковой длины.
+    (Векторизация без циклов)
+
+    Args:
+        a (numpy.ndarray): Первый вектор
+        b (numpy.ndarray): Второй вектор
+
+    Returns:
+        numpy.ndarray: Результат поэлементного сложения
+    """
+    result = a + b #np.add(a, b)
+    return result
+
+
+def scalar_multiply(vec, scalar):
+    """
+    Умножение вектора на число.
+
+    Args:
+        vec (numpy.ndarray): Входной вектор
+        scalar (float/int): Число для умножения
+
+    Returns:
+        numpy.ndarray: Результат умножения вектора на скаляр
+    """
+    result = vec * scalar
+    return result
+
+
+def elementwise_multiply(a, b):
+    """
+    Поэлементное умножение.
+
+    Args:
+        a (numpy.ndarray): Первый вектор/матрица
+        b (numpy.ndarray): Второй вектор/матрица
+
+    Returns:
+        numpy.ndarray: Результат поэлементного умножения
+    """
+    result = a * b
+    return result
+
+
+def dot_product(a, b):
+    """
+    Скалярное произведение.
+
+    Args:
+        a (numpy.ndarray): Первый вектор
+        b (numpy.ndarray): Второй вектор
+
+    Returns:
+        float: Скалярное произведение векторов
+    """
+
+    return np.dot(a, b)
+
+
+# ============================================================
+# 3. МАТРИЧНЫЕ ОПЕРАЦИИ
+# ============================================================
+
+def matrix_multiply(a, b):
+    """
+    Умножение матриц.
+
+    Args:
+        a (numpy.ndarray): Первая матрица
+        b (numpy.ndarray): Вторая матрица
+
+    Returns:
+        numpy.ndarray: Результат умножения матриц
+    """
+
+    return np.matmul(a, b)
+
+
+def matrix_determinant(a):
+    """
+    Определитель матрицы.
+
+    Args:
+        a (numpy.ndarray): Квадратная матрица
+
+    Returns:
+        float: Определитель матрицы
+    """
+    result = np.linalg.det(a)
+    return result
+
+
+def matrix_inverse(a):
+    """
+    Обратная матрица.
+
+    Изучить:
+    https://numpy.org/doc/stable/reference/generated/numpy.linalg.inv.html
+
+    Args:
+        a (numpy.ndarray): Квадратная матрица
+
+    Returns:
+        numpy.ndarray: Обратная матрица
+    """
+    result = np.linalg.inv(a)
+    return result
+
+
+def solve_linear_system(a, b):
+    """
+    Решить систему Ax = b
+
+    Изучить:
+    https://numpy.org/doc/stable/reference/generated/numpy.linalg.solve.html
+
+    Args:
+        a (numpy.ndarray): Матрица коэффициентов A
+        b (numpy.ndarray): Вектор свободных членов b
+
+    Returns:
+        numpy.ndarray: Решение системы x
+    """
+    result = np.linalg.solve(a, b)
+
+
+# ============================================================
+# 4. СТАТИСТИЧЕСКИЙ АНАЛИЗ
+# ============================================================
+
+def load_dataset(path="data/students_scores.csv"):
+    """
+    Загрузить CSV и вернуть NumPy массив.
+
+    Args:
+        path (str): Путь к CSV файлу
+
+    Returns:
+        numpy.ndarray: Загруженные данные в виде массива
+    """
+
+    return pd.read_csv(path).to_numpy()
+
+
+def statistical_analysis(data):
+    """
+    Представьте, что данные — это результаты экзамена по математике.
+    Нужно оценить:
+    - средний балл
+    - медиану
+    - стандартное отклонение
+    - минимум
+    - максимум
+    - 25 и 75 перцентили
+
+    Args:
+        data (numpy.ndarray): Одномерный массив данных
+
+    Returns:
+        dict: Словарь со статистическими показателями
+    """
+    return {'mean': np.mean(data),
+            'median': np.median(data),
+            'std': np.std(data),
+            'min': np.min(data),
+            'max': np.max(data),
+            'percentile_25': np.percentile(data, 25),
+            'percentile_75': np.percentile(data, 75)
+            }
+
+
+def normalize_data(data):
+    """
+    Min-Max нормализация.
+
+    Формула: (x - min) / (max - min)
+
+    Args:
+        data (numpy.ndarray): Входной массив данных
+
+    Returns:
+        numpy.ndarray: Нормализованный массив данных в диапазоне [0, 1]
+    """
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+
+# ============================================================
+# 5. ВИЗУАЛИЗАЦИЯ
+# ============================================================
+
+def plot_histogram(data):
+    """
+    Построить гистограмму распределения оценок по математике.
+
+    Изучить:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html
+
+    Args:
+        data (numpy.ndarray): Данные для гистограммы
+    """
+    # Подсказка: используйте plt.hist(), добавьте заголовок, подписи осей,
+    # сохраните в папку plots с помощью plt.savefig()
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(data, bins=20, color='skyblue', edgecolor='black')
+
+    plt.title('Распределение оценок по математике')
+    plt.xlabel('Оценка')
+    plt.ylabel('Количество студентов')
+
+    plt.grid(axis='y', alpha=0.5)
+
+    plt.savefig('plots/math_scores_hist.png')
+
+    #plt.close()
+
+
+def plot_heatmap(matrix):
+    """
+    Построить тепловую карту корреляции предметов.
+
+    Изучить:
+    https://seaborn.pydata.org/generated/seaborn.heatmap.html
+
+    Args:
+        matrix (numpy.ndarray): Матрица корреляции
+    """
+    # Подсказка: используйте sns.heatmap(), добавьте заголовок, сохраните
+    pass
+
+
+def plot_line(x, y):
+    """
+    Построить график зависимости: студент -> оценка по математике.
+
+    Изучить:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+
+    Args:
+        x (numpy.ndarray): Номера студентов
+        y (numpy.ndarray): Оценки студентов
+    """
+    # Подсказка: используйте plt.plot(), добавьте заголовок, подписи осей,
+    # сохраните график
+    pass
+
